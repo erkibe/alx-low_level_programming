217b0c53f8eab9a3499163e859548102065e5294
erkibe <erkibe12@gmail.com>
2022-06-03 11:44:17 -0700

    task four

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Print the alphabet - 'e' and 'q'
+* Return: 0
+ **/
+int main(void)
+{
+		char abc = 'a';
+
+		while
+		(abc <= 'z')
+		{
+		if
+		(abc != 'e' && abc != 'q')
+		{
+				putchar(abc);
+		}
+				abc++;
+		}
+				putchar('\n');
+					return (0);
+}
+
