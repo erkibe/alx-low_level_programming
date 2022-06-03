dc3dc0f86fda4eb729e112f87a9091fe8b1c5930
erkibe <erkibe12@gmail.com>
2022-06-03 11:51:14 -0700

    task six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - void
+* Return: 0
+**/
+int main(void)
+{
+		int num = 48;
+
+			while
+			(num <= 57)
+			{
+				putchar(num);
+				num++;
+			}
+				putchar('\n');
+					return (0);
+}
