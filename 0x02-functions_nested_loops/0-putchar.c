147dc6b7f9350ea193cf76d3a5c2545e80da25f3
erkibe <erkibe12@gmail.com>
2022-06-08 01:13:55 -0700

    task zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  *main-main block
- *   *Description:print -putchar in a new line
- *    *Return:this program returns zero
- *     */
+*main-main block
+*Description:print -putchar in a new line
+*Return:this program returns zero
+**/
 int main(void)
 {
 	_putchar('_');
