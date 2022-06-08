b962c2ebca25b9c9305dc06304cd0c2ce86c6623
erkibe <erkibe12@gmail.com>
2022-06-08 06:13:57 -0700

    task thirteen

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,9 +10,10 @@ int main(void)
 		int i, res;
 
 			for	(i = 0; i < 1024; i++)
-				if
-				(i % 3 == 0 || i % 5 == 0)
+				if	(i % 3 == 0 || i % 5 == 0i)
+
 					res += i;
+
 					printf("%d\n", res);
 
 			return (0);
