440c6e0d488c9eb7bc018ecdc193029bc95a97e9
erkibe <erkibe12@gmail.com>
2022-06-08 06:29:06 -0700

    task fourteen

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,16 +9,17 @@ int main(void)
 
 			i = 0;
 				j = 1;
-					for	(count = 0; count < 50; count++)
+					for
+					(count = 0;
+					 count < 50;
+					 count++)
 							{
-										k = i + j;
-												i = j;
-														j = k;
-										printf("%lu", k);
-																		if	(count == 49)
-																			putchar('\n');
-																				else
-																							{
+									k = i + j;
+										i = j;
+											j = k;
+					printf("%lu", k);
+												
+					if	(count == 49)																	putchar('\n');																else																		{
 																								printf(", ");
 																							}
 																					}
