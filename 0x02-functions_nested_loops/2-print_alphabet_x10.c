acd3330bf7a3991e9497d579ba7f336739c40a9b
erkibe <erkibe12@gmail.com>
2022-06-08 01:28:00 -0700

    task two

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,16 @@ void print_alphabet_x10(void)
 {
 		int i;
 			char c;
-
-				for 
-				(i = 0; 
-				i < 10; 
+			
+				for
+				(i = 0;
+				i < 10;
 				++i)
 						{
-									for 
-									(c = 'a'; 
-									 c <= 'z'; 
-									 c++)
+									for
+									(c = 'a';
+									c <= 'z';
+									c++)
 						{
 						_putchar(c);
 																		}
